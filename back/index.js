b9b024dd95c9479fc20981aa2bae6c9b120f2a0e
Nishajangir0972 <nishajangir9302@gmail.com>
2023-10-22 22:41:26 -0700

    Connecting Mongodb Atlas

--- back/index.js
+++ back/index.js
@@ -3,16 +3,22 @@ import cors from "cors";
 import mongoose from "mongoose";
 import FormRouter from "./FromRouter.js";
 
+const DB = "mongodb+srv://nishajangir9302:nisha0972@cluster0.0q9mmfo.mongodb.net/records?retryWrites=true&w=majority"
+
+
+
+
 const app = express()
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/Form")
+// const connection = mongoose.connect("mongodb://127.0.0.1:27017/Form")
+
 
 app.use(cors())
 
 app.use(express.json())
 app.use("/form" , FormRouter)
 
-connection.then(()=>{
+mongoose.connect(DB).then(()=>{
     app.listen(8080 , ()=> console.log("server started at 8080"))
 })
 .catch((err)=> console.log("error"+ err))
